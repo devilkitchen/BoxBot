411fe382cc8d03e415ceb432f0751ecb04572ff4
devilkitchen <devilkitchenn@gmail.com>
2025-01-25 14:45:17 +0530

    Update terabox.py

--- terabox.py
+++ terabox.py
@@ -308,7 +308,7 @@ async def handle_message(client, message: Message):
     is_member = await is_user_member(client, user_id)
 
     if not is_member:
-        join_button = InlineKeyboardButton("Join", url="")
+        join_button = InlineKeyboardButton("Join", url="https://t.me/+LR1EON0xyrllZjQ1")
         reply_markup = InlineKeyboardMarkup([[join_button]])
         await message.reply_text("You must join my channel to use me.", reply_markup=reply_markup)
         return
